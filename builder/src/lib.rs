d8ce6ec14650c16ccf9968f91fd9e0f76dc30b09
realth000 <a598919335@gmail.com>
2023-06-08 18:54:49 +0800

    Init builder/02-create-builders

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,36 @@
 use proc_macro::TokenStream;
 
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    TokenStream::new()
+    let ast = parse_macro_input!(input as DeriveInput);
+    let ident = ast.ident;
+    let builder_ident = Ident::new(
+        &format!("{}Builder", ident.to_string().as_str()),
+        Span::call_site(),
+    );
+    eprintln!("ident={}", ident);
+    quote!({
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #ident {
+            pub fn builder() -> $builderIdent {
+                #{ident}Builder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    })
+    .into()
 }
