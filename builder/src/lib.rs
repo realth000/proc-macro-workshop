f8acb5a7d5c55b854c3c5dab2eabfc2a4c2dae90
realth000 <a598919335@gmail.com>
2023-06-13 17:38:35 +0800

    Pass builder/06-optional-field

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
 
 use proc_macro2::Ident;
-use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Type};
+
+static OPTION_MODIFIER: [&str; 4] = ["<", ">", "(", ")"];
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -15,23 +17,70 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let mut field_vec: Vec<proc_macro2::TokenStream> = vec![];
     let mut empty_field_vec: Vec<proc_macro2::TokenStream> = vec![];
+    let mut field_method_vec: Vec<proc_macro2::TokenStream> = vec![];
+    let mut field_check_none_vec: Vec<proc_macro2::TokenStream> = vec![];
+    let mut field_build_vec: Vec<proc_macro2::TokenStream> = vec![];
 
-    match &ast.data {
-        Data::Struct(s) => {
-            if let Fields::Named(named_fields) = &s.fields {
-                for named_field in named_fields.named.iter() {
-                    let i = &named_field.ident;
-                    let t = &named_field.ty;
-                    field_vec.push(quote!(
-                        #i: std::option::Option<#t>
-                    ));
+    if let Data::Struct(s) = &ast.data {
+        if let Fields::Named(named_fields) = &s.fields {
+            for named_field in named_fields.named.iter() {
+                let i = &named_field.ident;
+                let t = &named_field.ty;
+                if let Type::Path(path) = t {
+                    let segments = &path.path.segments;
+                    if !segments.is_empty() && segments[0].ident.to_string().as_str() == "Option" {
+                        field_vec.push(quote!(
+                            #i: #t
+                        ));
+                        field_build_vec.push(quote!(
+                            #i: self.#i.clone()
+                        ));
+                        if let Some(type_name) = segments[0]
+                            .arguments
+                            .to_token_stream()
+                            .to_string()
+                            .split(' ')
+                            .find(|e| !OPTION_MODIFIER.contains(e))
+                        {
+                            let real_type = Ident::new(type_name, ident.span());
+                            field_method_vec.push(quote!(
+                                pub fn #i(&mut self, #i: #real_type) -> &mut Self {
+                                    self.#i = Some(#i);
+                                    self
+                                }
+                            ));
+                        } else {
+                            panic!("can not find type in Option")
+                        }
+                    } else {
+                        field_vec.push(quote!(
+                            #i: std::option::Option<#t>
+                        ));
+                        field_build_vec.push(quote!(
+                            #i: self.#i.clone().unwrap_or_default()
+                        ));
+                        field_method_vec.push(quote!(
+                            pub fn #i(&mut self, #i: #t) -> &mut Self {
+                                self.#i = Some(#i);
+                                self
+                            }
+                        ));
+                    }
                     empty_field_vec.push(quote!(
                         #i: None
                     ));
+                    field_check_none_vec.push(
+                        quote!(
+                                if self.#i.is_none() {
+                                    return Err(std::boxed::Box::<dyn std::error::Error>::from(format!("{} not set", "#i")))
+                                })
+                    );
                 }
+                // eprintln!("named_field: {:#?}", named_field);
             }
         }
-        _ => panic!("invalid derive type: not a struct"),
+    } else {
+        panic!("invalid derive type: not a struct")
     }
 
     // eprintln!("field_vec: {:#?}", field_vec);
@@ -51,46 +100,12 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
         impl #builder_ident {
             pub fn build(&mut self) -> Result<#ident, std::boxed::Box<dyn std::error::Error>> {
-                if self.executable.is_none() {
-                    return Err(std::boxed::Box::<dyn std::error::Error>::from(format!("{} not set", "executable")));
-                }
-                if self.args.is_none() {
-                    return Err(std::boxed::Box::<dyn std::error::Error>::from(format!("{} not set", "args")));
-                }
-                if self.env.is_none() {
-                    return Err(std::boxed::Box::<dyn std::error::Error>::from(format!("{} not set", "env")));
-                }
-                if self.current_dir.is_none() {
-                    return Err(std::boxed::Box::<dyn std::error::Error>::from(format!("{} not set", "current_dir")));
-                }
                 Ok(#ident{
-                    executable: self.executable.clone().unwrap(),
-                    args: self.args.clone().unwrap(),
-                    env: self.env.clone().unwrap(),
-                    current_dir: self.current_dir.clone().unwrap(),
+                    #(#field_build_vec,)*
                 })
-
             }
-
-            pub fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-
-            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-
-            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-
-            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
+            #(#field_method_vec)*
         }
-    ).into()
+    )
+    .into()
 }
