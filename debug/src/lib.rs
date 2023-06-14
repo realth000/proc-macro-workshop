5db9f7fa6dce42964ceec21af0883521fa757be0
realth000 <a598919335@gmail.com>
2023-06-14 16:23:40 +0800

    Pass debug/01-parse

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,7 +2,5 @@ use proc_macro::TokenStream;
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    TokenStream::new()
 }
