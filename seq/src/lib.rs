8d20b42dca10932a733275feff673deca692ae79
realth000 <a598919335@gmail.com>
2023-06-21 11:04:24 +0800

    Pass seq/02-parse-body

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::{TokenStream, TokenTree};
 
-use proc_macro2::{Ident, Span};
-use quote::{quote, ToTokens};
+use proc_macro2::{Delimiter, Ident, Literal, Span};
+use quote::{quote, ToTokens, TokenStreamExt};
 use syn::{parse_str, ExprBlock};
 
 macro_rules! compile_error {
@@ -57,9 +57,20 @@ pub fn seq(input: TokenStream) -> TokenStream {
         }
     };
 
+    // Parse and expand loop body.
     let loop_body = match parse_str::<ExprBlock>(&token_vec[6].1) {
         Ok(ExprBlock { block, .. }) => match first_group(block.to_token_stream()) {
-            Some(group) => group,
+            Some(group) => {
+                match apply_loop(start_value, end_value, &variable_name, &group.stream()) {
+                    Some(v) => v,
+                    None => {
+                        return compile_error!(
+                            proc_macro2::Span::from(token_vec[6].0.span()),
+                            "failed to apply loop"
+                        );
+                    }
+                }
+            }
             None => {
                 return compile_error!(
                     proc_macro2::Span::from(token_vec[6].0.span()),
@@ -74,7 +85,10 @@ pub fn seq(input: TokenStream) -> TokenStream {
             );
         }
     };
-    panic!("AAAA ->{:#?}<-", quote!(#loop_body).to_token_stream());
+    // panic!(
+    //     "[test] {:#?}<-",
+    //     proc_macro2::TokenStream::from(loop_body.to_token_stream())
+    // );
 
     quote!(
        #loop_body
@@ -91,4 +105,53 @@ fn first_group(token_stream: proc_macro2::TokenStream) -> Option<proc_macro2::Gr
         };
     }
     None
+}
+
+fn apply_loop(
+    start: i32,
+    end: i32,
+    variable: &Ident,
+    token_stream: &proc_macro2::TokenStream,
+) -> Option<proc_macro2::TokenStream> {
+    let variable_ident = match variable.to_token_stream().into_iter().next() {
+        Some(proc_macro2::TokenTree::Ident(ident)) => ident,
+        _ => return None,
+    };
+    let mut ret = proc_macro2::TokenStream::new();
+    // Repeat expanding code (end-start) times, apply variable to i in every expand.
+    for i in start..end {
+        // For every `TokenTree` in token_stream, check, expand and append it to the tail of output.
+        for tt in token_stream.clone() {
+            ret.extend(replace_ident(&variable_ident, i, &tt.to_token_stream()));
+        }
+    }
+    Some(ret)
+}
+
+// Check token_stream, apply every variable to real value.
+fn replace_ident(
+    variable: &Ident,
+    value: i32,
+    token_stream: &proc_macro2::TokenStream,
+) -> proc_macro2::TokenStream {
+    let target_literal = Literal::i32_unsuffixed(value);
+    let mut ret = proc_macro2::TokenStream::new();
+    for tt in token_stream.clone().into_iter() {
+        match &tt {
+            proc_macro2::TokenTree::Group(group) => {
+                ret.extend(replace_ident(variable, value, &group.stream()));
+            }
+            proc_macro2::TokenTree::Ident(ident) => {
+                let mut x = proc_macro2::TokenStream::new();
+                x.append(target_literal.clone());
+                if *ident == *variable {
+                    ret.append(proc_macro2::Group::new(Delimiter::Parenthesis, x))
+                } else {
+                    ret.append(ident.clone());
+                }
+            }
+            _ => ret.append(tt),
+        }
+    }
+    ret
 }
