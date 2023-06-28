3342c9c21f73c7991e8d556ed8090d08c4f1cc5d
realth000 <a598919335@gmail.com>
2023-06-28 17:33:33 +0800

    Pass sort/01-parse-enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,16 @@
 use proc_macro::TokenStream;
 
+use syn::{parse_macro_input, Item};
+
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let _ = input;
 
-    unimplemented!()
+    eprintln!("args: {:#?}", args);
+    eprintln!("input: {:#?}", input);
+    let item = parse_macro_input!(input as Item);
+    eprintln!("item: {:#?}", item);
+
+    TokenStream::new()
 }
