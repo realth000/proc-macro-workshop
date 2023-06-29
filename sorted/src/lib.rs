16494f3eb5f53a458e425f8748dbbd27f5f1f926
realth000 <a598919335@gmail.com>
2023-06-29 13:36:34 +0800

    Pass sorted/03-out-of-order

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 
-use syn::{parse_macro_input, Item};
+use syn::{parse_macro_input, Ident, Item};
 
 macro_rules! compile_error {
     ($span: expr, $($arg: tt)*) => {
@@ -14,7 +14,7 @@ macro_rules! compile_error {
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let item = parse_macro_input!(input as Item);
-    let _ast = if let Item::Enum(item_enum) = item {
+    let item_enum = if let Item::Enum(item_enum) = item {
         item_enum
     } else {
         return compile_error!(
@@ -23,5 +23,19 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
         );
     };
 
+    // panic!("{:#?}", _ast.variants);
+
+    let mut all_ident: Vec<Ident> = item_enum.variants.iter().map(|e| e.ident.clone()).collect();
+    let all_ident_orig = all_ident.clone();
+    all_ident.sort();
+    if let Some((orig, sorted)) = all_ident_orig
+        .iter()
+        .zip(all_ident.iter())
+        .find(|(orig, sorted)| orig != sorted)
+    {
+        return compile_error!(sorted.span(), "{} should sort before {}", sorted, orig);
+    }
+
+    // panic!("all ident: {:#?}", all_ident);
     TokenStream::new()
 }
