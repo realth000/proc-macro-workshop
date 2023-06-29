ba8be39dc11852744c3d661ba7f1b51d155683a1
realth000 <a598919335@gmail.com>
2023-06-29 10:24:04 +0800

    Pass sorted/02-not-enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -2,15 +2,26 @@ use proc_macro::TokenStream;
 
 use syn::{parse_macro_input, Item};
 
+macro_rules! compile_error {
+    ($span: expr, $($arg: tt)*) => {
+        syn::Error::new($span, format!($($arg)*))
+            .to_compile_error()
+            .into()
+    };
+}
+
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
-
-    eprintln!("args: {:#?}", args);
-    eprintln!("input: {:#?}", input);
     let item = parse_macro_input!(input as Item);
-    eprintln!("item: {:#?}", item);
+    let _ast = if let Item::Enum(item_enum) = item {
+        item_enum
+    } else {
+        return compile_error!(
+            proc_macro2::Span::call_site(),
+            "expected enum or match expression"
+        );
+    };
 
     TokenStream::new()
 }
